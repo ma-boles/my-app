246b97c14c0adaa4fa9e60ef631a4ce6781364e0
ma-boles <ma_boles@hotmail.com>
2023-09-12 18:43:40 -0700

    git commit -m "new file"

---
+++ src/TestimonialCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function TestimonialCard() {
+    return (
+        <>
+        <div className="card">
+            <h6>Rating</h6>
+            <div className="midCard">
+                <img></img>
+                <h6>Name</h6>
+            </div>
+            <h6>Review text</h6>
+        </div>
+        <div className="card">
+            <h6>Rating</h6>
+            <div className="midCard">
+                <img></img>
+                <h6>Name</h6>
+            </div>
+            <h6>Review text</h6>
+        </div>
+        <div className="card">
+            <h6>Rating</h6>
+            <div className="midCard">
+                <img></img>
+                <h6>Name</h6>
+            </div>
+            <h6>Review text</h6>
+        </div>
+        <div className="card">
+            <h6>Rating</h6>
+            <div className="midCard">
+                <img></img>
+                <h6>Name</h6>
+            </div>
+            <h6>Review text</h6>
+        </div>
+    </>
+    )
+}
