5dc4183cda2166eea0dedb2c81187c6f0ce22104
ma-boles <ma_boles@hotmail.com>
2023-09-12 18:44:10 -0700

    git commit -m "new file"

---
+++ src/MenuCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import GreekSalad from "./images/greek salad.jpg";
+import Bruschetta from "./images/Mario and Adrian A.jpg";
+import LemonDessert from "./images/lemon dessert.jpg";
+
+export default function MenuCard () {
+    return (   
+    <>
+    <div className="menuCard">
+        <img src={GreekSalad} className="img--menu"></img>
+        <div className="heading">
+            <h4>Greek salad</h4>
+            <p>$12.99</p>
+        </div>
+        <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+        <h6>Order a delivery</h6>
+    </div>
+
+    <div className="menuCard">
+        <img src={Bruschetta} className="img--menu"></img>
+        <div className="heading">
+            <h4>Bruschetta</h4>
+            <p>$5.99</p>
+        </div>
+        <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
+        <h6>Order a delivery</h6>
+    </div>
+
+    <div className="menuCard">
+        <img src={LemonDessert} className="img--menu"></img>
+        <div className="heading">
+            <h4>Lemon Dessert</h4>
+            <p>$5.99</p>
+        </div>
+        <p>This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+        <h6>Order a delivery</h6>
+    </div>
+        
+    </>
+    )
+}
