584574a209179980f71c88e653286f41ec7175f5
ma-boles <ma_boles@hotmail.com>
2023-09-17 15:30:50 -0700

    git commit -m "updated to working form"

--- src/Pages/BookingPage.js
+++ src/Pages/BookingPage.js
@@ -7,19 +7,30 @@ import BookingForm from "../BookingForm";
 
 export default function BookingPage() {
 
-    let date
-    let reducer
+    const [date, setDate] = React.useState(new Date())
+
     
     function initializeTimes(date) {
-
     } 
 
     function updateTimes(date){
-
+        const dateObj = new Date(date)
     }
 
     function submitForm(formData) {
         console.log("test")
+    }
+
+    function reducer (state, action) {
+        let newState;
+        switch (action.type) {
+            case "update_times":
+                const newDate = new Date(action.payload);
+                newState = updateTimes(newDate)
+                break;
+                default: throw new Error()
+        }
+        return newState
     }
     const [ availableTimes, dispatch ] = useReducer(reducer, initializeTimes(date))
 
