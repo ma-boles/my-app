a0ef029d38891c69ddc6c1038ebefa5e7b9fd7e6
ma-boles <ma_boles@hotmail.com>
2023-09-18 17:57:03 -0700

    git commit -m "unit tests"

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import BookingPage from "./Pages/BookingPage";
 
+/*
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+});*/
+
+test("Renders the BookingPage heading", () => {
+  render(<BookingPage />);
+  const headingElement = screen.getByText("Reservations Form");
+  expect(headingElement).toBeInTheDocument();
+})
+
+test("Renders updateTimes", () => {
+  const updateTimes = jest.fn();
+  render (<BookingPage onSubmit={updateTimes} />)
+})
+
+test("Renders initializeTimes", () => {
+  const initializeTimes = jest.fn();
+  render (<BookingPage onSubmit = {initializeTimes}/>)
+})
