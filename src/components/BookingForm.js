0a0f07976acb5fef300e9b5b87062d7919aa1396
ma-boles <ma_boles@hotmail.com>
2023-09-25 17:47:39 -0700

    git commit -m "form validation updates"

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,33 +1,23 @@
 import React from "react";
 import "../styles/App.css"
-import * as Yup from "yup";
-import { Formik, Form, Field } from "formik";
 
-const SignupSchema = Yup.object().shape({
-    fname: Yup.string()
-    .min(3)
-    .max(15)
-    .required("required"),
-    lname: Yup.string()
-    .min(3)
-    .max(15)
-    .required("required"),
-    email: Yup.string().email("Invalid email").required("Required")     
-})
 
 export default function BookingForm ({ /*availableTimes, occasions,*/ dispatch, submitForm }) {
 
+const initialFormData = {
+    fname: "", 
+    lname: "",
+    email: "",
+    phone: "",
+    time: "",
+    date: "",
+    guests: "0",
+    occasion: ""
+};
+    const [ formData, setFormData ] = React.useState(initialFormData);
 
-    const [ formData, setFormData ] = React.useState({
-        /*fname: "", 
-        lname: "",
-        email: "",*/
-        phone: "",
-        time: "",
-        date: "",
-        guests: "0",
-        occasion: ""
-    })
+    const [ errors, setErrors ] = React.useState({});
+    const [ submitted,setSubmitted ] = React.useState(false);
 
     const handleChangeForm = (event) => {
         const { name, value } = event.target
@@ -46,11 +36,63 @@ export default function BookingForm ({ /*availableTimes, occasions,*/ dispatch, 
         dispatch({ type: "update_times", payload: value })
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        submitForm(formData)
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-        console.log(formData)
+        if(validateForm()) {
+            console.log("Form data submitted:", formData);
+
+            setFormData(initialFormData);
+            setErrors({});
+            setSubmitted(true);
+
+        } else {
+            console.log("Form data contains errors:", errors);
+        }
+    };
+
+    const validateForm = () => {
+        const newErrors = {};
+
+        if(!formData.fname.trim()) {
+            newErrors.fname = "Name is required";
+        }
+        if(!formData.lname.trim()) {
+            newErrors.lname = "Name is required";
+        }
+        if(!formData.email.trim()) {
+            newErrors.email = "Email is required";
+        } else if (!validateEmail(formData.email)) {
+            newErrors.email = "Invalid email format";
+        }
+        
+        if(!formData.phone.trim()) {
+            newErrors.phone = "Phone number is required";
+        } else if (!validatePhone(formData.phone)) {
+            newErrors.phone = "Invalid phone number format";
+        }
+        
+        if(!formData.time.trim()) {
+            newErrors.time = "Please select time";
+        }
+        
+        if(!formData.guests.trim()) {
+            newErrors.guests = "Parties of 1 or more";
+        }
+
+        setErrors(newErrors);
+
+        return Object.keys(newErrors).length === 0;
+    };
+
+    const validateEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
+    const validatePhone = (phone) => {
+        const phoneRegex = /^[0-9-]+$/;
+        return phoneRegex.test(phone);
     }
 
     const currentDate = new Date().toISOString().split("T") [0]
@@ -61,73 +103,56 @@ export default function BookingForm ({ /*availableTimes, occasions,*/ dispatch, 
     const options = availableTimes.map(time => <option key={time}>{time}</option>)
     const occasionsOptions = occasions.map(occasion => <option key={occasion}>{occasion}</option>)
 
-/*
-    function checkform() {
-        const formElements = document.forms["form"].elements;
-        const submitBtn = document.getElementById("submit");
-
-        const submitBtnActive = Array.from(formElements).every(inputElement => inputElement.value.trim() !== "");
-
-        submitBtn.disabled = !submitBtnActive
-    }*/
+   
 
     return(
         <>
-        <Formik
-                initialValues={{
-                    fname: "",
-                    lname: "",
-                    email: "",
-                }}
-                validationSchema={SignupSchema}
-                onSubmit= {values => {
-                    console.log(values);
-                }}>
-                {({ errors, touched }) => (
-
                 
-        <Form name="form" onSubmit={handleSubmit}>
+        <form name="form" onSubmit={handleSubmit}>
             
                 <div>
                     <label htmlFor="name">First Name:</label><br />
-                    <Field type="text" id="fname" name="fname" required value={formData.fname} onChange={handleChangeForm}/>
-                    {errors.fname && touched.fname ? (
-                        <div>{errors.fname}</div>) : null}
+                    <input type="text" id="fname" name="fname" required value={formData.fname} onChange={handleChangeForm}/>
+                    {errors.fname && !submitted && <div className="error">{errors.fname}</div>}
                 </div>
 
                 <div>
                     <label htmlFor="name">Last Name:</label><br />
                     <input type="text" id="lname" name="lname" required value={formData.lname} onChange={handleChangeForm}/>
-                    {errors.lname && touched.lname  ? (<div>{errors.lname }</div>) : null}
+                    {errors.lname && !submitted && <div className="error">{errors.lname}</div>}
                 </div>
 
                 <div>
                     <label htmlFor="email">Email:</label><br />
                     <input type="email" id="email" name="email" required value={formData.email} onChange={handleChangeForm} ></input>
-                    {errors.email && touched.email ? <div>{errors.email}</div> : null }
+                    {errors.email && !submitted && <div className="error">{errors.email}</div>}
                 </div>
 
                 <div>
                     <label htmlFor="phone">Phone:</label>
-                    <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="(xxx) xxx-xxxx" required min="10" max="20" value={formData.phone} onChange={handleChangeForm} onKeyUp={checkform()}></input>
+                    <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="(xxx) xxx-xxxx" required min="10" max="20" value={formData.phone} onChange={handleChangeForm} ></input>
+                    {errors.phone && !submitted && <div className="error">{errors.phone}</div>}
                 </div>
 
                 <div>
                     <label htmlFor="date">Date:</label>
-                    <input type="date" id="date" name="date" required value={formData.date} min={currentDate} onChange={handleChangeDate} onKeyUp={checkform()}/>
+                    <input type="date" id="date" name="date" required value={formData.date} min={currentDate} onChange={handleChangeDate} />
+                    {errors.date && !submitted && <div className="error">{errors.date}</div>}
                 </div>
 
                 <div>
                     <label htmlFor="time">Time:</label><br />
-                    <select id="time" name="time" required value={formData.time} onChange={handleChangeForm} onKeyUp={checkform()}>
+                    <select id="time" name="time" required value={formData.time} onChange={handleChangeForm} >
                         {options} 
                     </select>
+                    {errors.time && !submitted && <div className="error">{errors.time}</div>}
                 </div>
 
                 <div>
                     <label htmlFor="guests">Guests:</label><br />
-                    <input type="number" id="guests" name="guests" required placeholder="1" min="1" max="20" value={formData.guests} onChange={handleChangeForm} onKeyUp={checkform()}/>
-                </div>
+                    <input type="number" id="guests" name="guests" required placeholder="1" min="1" max="20" value={formData.guests} onChange={handleChangeForm}/>
+                    {errors.guests && !submitted && <div className="error">{errors.guests}</div>}
+               </div>
 
                 <div>
                     <label htmlFor="occasion">Occasion:</label><br />
@@ -137,12 +162,10 @@ export default function BookingForm ({ /*availableTimes, occasions,*/ dispatch, 
                 </div>
      
                 <div>
-                    <button id="submit" type="submit" value="Reservation" onClick={handleSubmit} aria-label="reserve" disabled="disabled">Reserve</button>
+                    <button id="submit" type="submit" value="Reservation" aria-label="reserve" >Reserve</button>
                 </div>
 
-            </Form>
-            )}
-            </Formik>
+            </form>
         </>
     )
 }
